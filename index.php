c92fc5d8e88f1deec4984e64aca54bcea76096a6
Davidson228 <davidarinsh@gmail.com>
2017-09-29 15:34:56 +0300

    tets

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ require 'vendor/autoload.php';
 echo 'hello my name is david';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-//
+$button = $app->add('Button');
+$button->set('Add Inovice');
+$button->icon = 'plus';
