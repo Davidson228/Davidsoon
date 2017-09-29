fe9938717edbd8d0267b1c6cb81721af829fcfde
Davidson228 <davidarinsh@gmail.com>
2017-09-29 15:49:21 +0300

    test

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ echo 'hello my name is david';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
 $button = $app->add('Button');
-$button->set('Start');
+$button->set('E-klase');
 $button->icon = 'plus';
 $button->link('https://www.e-klase.lv');
