ce3804573fbaa294b0a044924af9a6d2d2fa9081
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2017-10-08 13:11:47 +0300

    test

--- index.php
+++ index.php
@@ -8,19 +8,25 @@ $button->set('E-klase');
 $button->icon = 'find';
 $button->link('https://www.e-klase.lv');
 $button->addClass('small red');
+
 $button = $app->add('Button');
 $button->set('Heroku');
 $button->icon = 'rebel';
 $button->link('https://www.heroku.com');
 $button->addClass('orange');
+
 $button = $app->add('Button');
 $button->set('Steam');
 $button->icon = 'steam square';
 $button->link('http://store.steampowered.com');
 $button->addClass('big yellow');
+
 $button = $app->add(['Button', 'Semantic', 'iconRight'=>'right arrow']);
 $button->addClass('huge green');
 $button->link('https://semantic-ui.com');
-$button = $app->add(['Button', 'Apple Shop', 'iconRight'=>'right arrow']);
+
+$button = $app->add('Button');
+$button->set('Apple shop');
 $button->icon = 'apple';
+$button->link('https://www.apple.com');
 $button->addClass('small teal');
