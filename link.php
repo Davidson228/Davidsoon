55d35adcddd061104e080b2a0ddc017e3c243c5b
Ignatyeff <boroda42017@gmail.com>
2017-10-13 15:22:49 +0300

    rrerrrrr

--- link.php
+++ link.php
@@ -4,3 +4,4 @@ echo 'hello my name is David';
 $app = new \atk4\ui\App('Galijo');
 $app->initLayout('Centered');
 //$button -> link('')
+$label = $app->app(['Label', 'Astralis','icon'=>'fort awesome']);
