2583c377b298edfa270d86a8ac7b5b7b48149b6d
Ignatyeff <boroda42017@gmail.com>
2017-10-13 15:39:51 +0300

    teets

--- link.php
+++ link.php
@@ -4,4 +4,4 @@ echo 'hello my name is David';
 $app = new \atk4\ui\App('Galijo');
 $app->initLayout('Centered');
 //$button -> link('')
-$label = $app->add(['Label', 'Astralis','big brown','icon'=>'fort awesome']);
+$label = $app->add(['Label', 'Astralis','big brown','icon'=>'fort awesome','detail' =>'hello' ]);
