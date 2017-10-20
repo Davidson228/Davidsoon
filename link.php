739d59717b21e4c3c5dd170850ca8e02a614546e
Ignatyeff <boroda42017@gmail.com>
2017-10-20 15:36:58 +0300

    e

--- link.php
+++ link.php
@@ -5,11 +5,55 @@ $app->initLayout('Centered');
 //$button -> link('')
 $label = $app->add(['Label', 'Astralis','big brown','icon'=>'fort awesome','detail' =>'hello' ]);
 $text = $app->add(['Text','Astralis']);
-$a = 0;
-if ($a != 0) {
+
+
+$a = 1;
+if ($a == 0) {
 $name  = 'Верно!';
 } else {
 $name = 'Неверно!';
 }
 $label = $app->add(['Label', $name,'big pink','icon'=>'fort awesome','detail' =>'Сливаю не сливаемое, тащу нетащимое' ]);
 $text = $app->add(['Text','Navi']);
+
+$a = 0;
+if ($a == 0) {
+$name  = 'Верно!';
+} else {
+$name = 'Неверно!';
+}
+
+$a = 0;
+if ($a != 0) {
+$name  = 'Верно!';
+} else {
+$name = 'Неверно!';
+}
+
+$a = 0;
+if ($a != 0) {
+$name  = 'Верно!';
+} else {
+$name = 'Неверно!';
+}
+
+$a = 0;
+if ($a != 0) {
+$name  = 'Верно!';
+} else {
+$name = 'Неверно!';
+}
+
+$a = 0;
+if ($a != 0) {
+$name  = 'Верно!';
+} else {
+$name = 'Неверно!';
+}
+
+$a = 0;
+if ($a != 0) {
+$name  = 'Верно!';
+} else {
+$name = 'Неверно!';
+}
