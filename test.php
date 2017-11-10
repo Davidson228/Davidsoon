6d80a21e7379a813f8fcc116ef0bf119676b1ae7
Ignatyeff <boroda42017@gmail.com>
2017-11-10 16:04:10 +0200

    test

--- test.php
+++ test.php
@@ -7,3 +7,14 @@ $min = $_GET['min'];
 $max = $_GET['max'];
 $mid =round ( ($max + $min) / 2);
 $label = $app->add(['Label', 'Твоё число это '.$mid.' ?', 'black','icon'=>'alarm']);
+
+$button1 = $app->add('Button');
+$button1->set('Да это моё число');
+
+$button2 = $app->add('Button');
+$button2->set('Нет моё число больше');
+$button2->link(['test', 'min'=>$mid, 'max'=>$max]);
+
+$button3 = $app->add('Button');
+$button3->set('Нет моё число меньше');
+$button2->link(['test', 'min'=>$min, 'max'=>$mid]);
