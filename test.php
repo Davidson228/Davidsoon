cdeae1a636b84341d064ea3084e529611cbfe540
Ignatyeff <boroda42017@gmail.com>
2017-11-17 15:21:15 +0200

    test

--- test.php
+++ test.php
@@ -3,6 +3,12 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Galijo');
 $app->initLayout('Centered');
 
+if ($max-$min==1){
+$text = $app -> add (['Text','Ошибка']);
+$button4=$app->$add(['Button','RESTART']);
+$button4->link(['index', 'min'=>1, 'max'=>100]);
+}
+
 $min = $_GET['min'];
 $max = $_GET['max'];
 $mid =round ( ($max + $min) / 2);
