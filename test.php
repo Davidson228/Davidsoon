5acf1bc4106014e7244a38123e40e83703be3164
Ignatyeff <boroda42017@gmail.com>
2017-11-17 15:57:54 +0200

    test

--- test.php
+++ test.php
@@ -5,6 +5,13 @@ $app->initLayout('Centered');
 
 $min = $_GET['min'];
 $max = $_GET['max'];
+
+if (($max-$min==1) and (ISSET($_GET['key']))){
+$text = $app -> add (['Text','Ошибка']);
+$button4=$app->add(['Button','RESTART']);
+$button4->link(['test', 'min'=>0, 'max'=>100]);
+}
+
 $mid =round ( ($max + $min) / 2);
 $label = $app->add(['Label', 'Твоё число это '.$mid.' ?', 'black','icon'=>'alarm']);
 
@@ -18,9 +25,3 @@ $button2->link(['test', 'min'=>$min, 'max'=>$mid]);
 $button3 = $app->add('Button');
 $button3->set('Нет моё число больше');
 $button3->link(['test', 'min'=>$mid, 'max'=>$max]);
-
-if ($max-$min==1){
-$text = $app -> add (['Text','Ошибка']);
-$button4=$app->add(['Button','RESTART']);
-$button4->link(['test', 'min'=>0, 'max'=>100]);
-}
