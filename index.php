d5748e0a9e695ec5b544d353c770d6fd81d76a91
Ignatyeff <boroda42017@gmail.com>
2017-12-01 15:53:24 +0200

    s

--- index.php
+++ index.php
@@ -7,22 +7,22 @@ $app->initLayout('Centered');
 
 
  $candy1 = new Candy;
- $candy1 -> name = 'Serenade';
- $candy1 -> colour = 'blue';
- $candy1 -> price = '8';
+ $candy1 ->name = 'Serenade';
+ $candy1 ->colour = 'blue';
+ $candy1 ->price = '8';
  $label = $app->add(['Label', 'Serenade', 'blue','icon'=>'alarm']);
 
  $candy2 = new Candy;
- $candy2 -> name = 'Lacishi';
- $candy2 -> colour = 'teal';
- $candy2 -> price = '5';
+ $candy2 ->name = 'Lacishi';
+ $candy2 ->colour = 'teal';
+ $candy2 ->price = '5';
 
  $candy3 = new Candy;
- $candy3 -> name = 'Lukss';
- $candy3 -> colour = 'red';
- $candy3 -> price = '9';
+ $candy3 ->name = 'Lukss';
+ $candy3 ->colour = 'red';
+ $candy3 ->price = '9';
 
  $candy4 = new Candy;
- $candy4 -> name = 'Vaverite';
- $candy4 -> colour = 'green';
- $candy4 -> price = '7';
+ $candy4 ->name = 'Vaverite';
+ $candy4 ->colour = 'green';
+ $candy4 ->price = '7';
