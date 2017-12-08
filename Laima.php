548d766e3ddcf102f74bf06cefaea1a90d1cf641
SizedMaks24 <shvint74@gmail.com>
2017-12-08 15:34:20 +0200

    ede

--- Laima.php
+++ Laima.php
@@ -3,4 +3,8 @@ CLass Candy {
   public $colour;
   public $price;
   public $name;
+  public function AllPrice($weight){
+  $answer = $this->price * $weight;
+  return $answer;
+}
 }
--- index.php
+++ index.php
@@ -10,19 +10,24 @@ $app->initLayout('Centered');
  $candy1 ->name = 'Serenade';
  $candy1 ->colour = 'blue';
  $candy1 ->price = '8';
- $label = $app->add(['Label', 'Serenade', 'blue','icon'=>'alarm']);
+ $label1 = $app->add(['Label','detail'=>$candy1 ->price , 'blue','detail'=>$candy1 ->price ,'icon'=>'alarm' ]);
 
  $candy2 = new Candy;
  $candy2 ->name = 'Lacishi';
  $candy2 ->colour = 'teal';
  $candy2 ->price = '5';
+ $label2 = $app->add(['Label', 'detail'=>$candy2 ->price, 'teal','detail'=>$candy2 ->price ,'icon'=>'bitcoin' ]);
 
  $candy3 = new Candy;
  $candy3 ->name = 'Lukss';
  $candy3 ->colour = 'red';
  $candy3 ->price = '9';
+ $label3 = $app->add(['Label','detail'=>$candy3 ->price,, 'red','detail'=>$candy3 ->price ,'icon'=>'empire' ]);
+
+
 
  $candy4 = new Candy;
  $candy4 ->name = 'Vaverite';
  $candy4 ->colour = 'green';
  $candy4 ->price = '7';
+ $label4 = $app->add(['Label','detail'=>$candy4 ->price,, '','detail'=>$candy4 ->price ,'icon'=>'candy' ]);
