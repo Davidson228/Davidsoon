919abf4df09d14c900e0ea4be81c12cf6e314d95
Ignatyeff <boroda42017@gmail.com>
2018-01-26 14:44:15 +0200

    gsgsgdhuswgf8uehvfuo

--- friendtest.php
+++ friendtest.php
@@ -33,7 +33,7 @@ $app->initLayout('Centered');
  */
 
  $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=fdb;for_сolibri;host=localhost','root','');
+\atk4\data\Persistence_SQL('mysql:dbname=fdb;host=localhost','root','');
 class Friends extends \atk4\data\Model {
   public $table = 'friends';
   function init() {
